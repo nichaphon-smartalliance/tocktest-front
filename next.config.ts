62641b2ca5ec975b86a7d4a80f69b692d5777be0
dong <nichaphon@smartalliance.co.th>
2026-06-09 14:21:07 +0700

    feat: add .env.production to .gitignore and update next.config.ts output setting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["antd", "@ant-design/icons", "@ant-design/cssinjs"],
+  output: "standalone",
 };
 
 export default nextConfig;
