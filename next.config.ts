c08e4f6479f4bbc90693c71ad1669d8b2357f280
dong <nichaphon@smartalliance.co.th>
2026-06-24 11:26:14 +0700

    feat(config): add allowedDevOrigins and update output settings

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,27 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
   transpilePackages: ["@heroui/react"],
   modularizeImports: {
     "lucide-react": {
       transform: "lucide-react/dist/esm/icons/{{kebabCase member}}",
     },
   },
+  allowedDevOrigins: [
+    "localhost",
+    "https://did.smartalliance.co.th",
+    "did.smartalliance.co.th",
+    "localhost:3001",
+    "192.168.33.162",
+    "192.168.33.162:3001",
+    "192.168.33.166",
+    "192.168.33.160",
+    "192.168.33.166:3000",
+    "192.168.33.160:6601",
+    "154.197.124.206",
+  ],
   async redirects() {
     return [
       // Repo root has no page; send it to the first tab.
